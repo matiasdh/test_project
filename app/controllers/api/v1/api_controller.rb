dcfbe87a22a5a2759a2cd3170c3d4d0f5dfd9256
TopTier labs <toptierlabs@TopTiers-iMac-2.local>
2015-03-05 12:43:20 -0200

    Sessions controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::ApiController < ApplicationController
+  before_action :authenticate_user!
 
   class AuthenticationException < Exception; end
 
-  def authenticate_user
+  def authenticate_user!
     @current_user = User.find_by session_token: params[:token]
     raise AuthenticationException unless @current_user
   end

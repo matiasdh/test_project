f6659bbad15dea960cbe9340e697d0a90b872d05
TopTier labs <toptierlabs@TopTiers-iMac-2.local>
2015-03-05 12:25:57 -0200

    APIController added and updated the gem file

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ApiController < ApplicationController
+
+  class AuthenticationException < Exception; end
+
+  def authenticate_user
+    @current_user = User.find_by session_token: params[:token]
+    raise AuthenticationException unless @current_user
+  end
+
+  rescue_from AuthenticationException do |exception|
+    render json: exception, status: 401
+  end
+end
